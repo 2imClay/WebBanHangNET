d73ebb2b6ae1b4766f49d85beeca744cf61f5ca7
HungNg <21130367@st.hcmuaf.edu.vn>
2025-01-03 16:22:40 +0700

    show products in categories

--- WebBanHang/Controllers/CategoriesController.cs
+++ WebBanHang/Controllers/CategoriesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBanHang.Models;
 
 namespace WebBanHang.Controllers
 {
@@ -11,7 +12,19 @@ namespace WebBanHang.Controllers
         // GET: Categories
         public ActionResult Index()
         {
-            return View();
+            // Load all products from the database
+            using (var db = new MyDbContext())
+            {
+                //var products = db.Products.ToList(); // Lấy tất cả các sản phẩm từ cơ sở dữ liệu
+
+                var products = new List<Product>
+    {
+                new Product { Name = "Product 1", Price = 100 },
+                 new Product { Name = "Product 2", Price = 200 }
+    };
+
+                return View(products); // Trả về danh sách sản phẩm cho View
+            }
         }
     }
 }
--- WebBanHang/Controllers/ProductController.cs
+++ WebBanHang/Controllers/ProductController.cs
@@ -15,7 +15,14 @@ namespace WebBanHang.Controllers
             // Load all products from the database
             using (var db = new MyDbContext())
             {
-                var products = db.Products.ToList(); // Lấy tất cả các sản phẩm từ cơ sở dữ liệu
+                //var products = db.Products.ToList(); // Lấy tất cả các sản phẩm từ cơ sở dữ liệu
+
+                var products = new List<Product>
+    {
+                new Product { Name = "Product 1", Price = 100 },
+                 new Product { Name = "Product 2", Price = 200 }
+    };
+
                 return View(products); // Trả về danh sách sản phẩm cho View
             }
         }
